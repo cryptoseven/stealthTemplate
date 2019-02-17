3780164e9372c356117300b6df812c17014ec48a
andreas <andreasw87@gmail.com>
2019-02-17 21:26:21 +0100

    Add Objective Pickup

---
+++ Source/FPSGame/Private/FPSOBjectiveActor.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSOBjectiveActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFPSOBjectiveActor::AFPSOBjectiveActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(meshComp);
+	meshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	sphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	sphereComp->SetupAttachment(meshComp);
+	sphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	sphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	sphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+// Called when the game starts or when spawned
+void AFPSOBjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+	playEffects();
+	
+}
+
+void AFPSOBjectiveActor::playEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this,pickupFX, GetActorLocation());
+}
+
+// Called every frame
+void AFPSOBjectiveActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFPSOBjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	playEffects();
+}
+
---
+++ Source/FPSGame/Public/FPSOBjectiveActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSOBjectiveActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API AFPSOBjectiveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSOBjectiveActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* meshComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* sphereComp;
+	void playEffects();
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	UParticleSystem* pickupFX;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+};
